c9030f75d967864d87ca8b1c5d80c38377eedf03
Caitlin Conway <coolcac@gmail.com>
2020-08-10 11:41:07 -0500

    setup part 1 done

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "pug");
+const morgan = require("morgan");
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+const csrfProtection = require("csurf")({ cookie: true });
+app.use(csrfProtection);
+app.use("/public", express.static("public"));
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+app.use((req, res, next) => {
+  res.render("error-page");
+});
+module.exports = app;
