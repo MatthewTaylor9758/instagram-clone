ce96ec28e976131a184c86620daa66a628a64d47
Caitlin Conway <coolcac@gmail.com>
2020-08-16 14:09:38 -0500

    delete button fixed

--- public/js/events.js
+++ public/js/events.js
@@ -1,21 +1,21 @@
 // import User from "../../db/models";
-window.addEventListener('click', async (e) => {
+window.addEventListener("click", async (e) => {
   let regex = /delete-button-\d+/;
   // console.log(User.id);
   if (regex.test(e.target.id)) {
-    console.log(e.target.id)
+    console.log(e.target.id);
     let photoId = e.target.id.slice(14, e.target.id.length);
     console.log(photoId);
-    const res = await fetch(`../api/pictures/${photoId}`,{
-      method: 'DELETE',
+    const res = await fetch(`/api/pictures/${photoId}`, {
+      method: "DELETE",
       headers: {
-        'Content-type': 'application/json'
-      }
-    })
+        "Content-type": "application/json",
+      },
+    });
     window.location.href = window.location.pathname;
-    // const pic = await res.text();
+    const pic = await res.text();
     // console.log(pic);
   }
   // console.log(e.target.id);
-})
+});
 // console.log(window);
